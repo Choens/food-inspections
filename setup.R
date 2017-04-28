0b0ff57e746306dff7835f3dbb9af7e523032b78
Andy Choens <andy.choens@gmail.com>
2017-04-28 07:41:49 -0400

    Finished the setup instructions.
    Almost forgot about chroloplethr,
    
     Changes to be committed:
    	modified:	setup.R

--- setup.R
+++ setup.R
@@ -5,8 +5,14 @@
 ##
 ## #############################################################################
 
-packages <- c("dplyr",
+packages <- c("devtools",
+              "choroplethr",      
+              "dplyr",
               "tidyr",
               "rmarkdown",
               "RSocrata")
 install.packages(packages)
+
+## This step will take a few minutes. We have to all download a big file or three.
+library(devtools)
+install_github('arilamstein/choroplethrZip@v1.5.0')
